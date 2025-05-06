4da41e96bf82c13e22bc8b7849aa74bdc290497b
Riick6 <rick6.uwu@gmail.com>
2025-05-06 17:17:16 -0500

    el navbar del usuario e inicio

---
+++ frontend/navbar.php
@@ -0,0 +1,64 @@
+<!-- navbar.php -->
+<div class="container mt-3">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <img src="../backend/img/logo_sistema.png" alt="Portal de Denuncias" class="logo">
+            </div>
+            <div class="col-md-6 text-end">
+                <input type="search" class="form-control buscador" placeholder="¿Qué deseas buscar?">
+            </div>
+        </div>
+    </div>
+    
+<nav class="navbar navbar-expand-lg navbar-dark mt-3">
+        <div class="container header-nav">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item"><a class="nav-link" href="inicio.php"><i style="font-size: 20px;" class="bi bi-house-fill"></i></a></li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Servicios</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">Registrar Denuncia</a></li>
+                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">Ver Denuncias</a></li>
+                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">Consultar Estado</a></li>
+                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">Mapa del Crimen</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Nosotros</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">¿Quiénes Somos?</a></li>
+                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">Mision y Vision</a></li>
+                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">Equipo de trabajo</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Blog</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">Noticias y Actualizaciones</a></li>
+                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">Consejo sobre denuncias</a></li>
+                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">Casos de éxito</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Seguridad</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">Privacidad y Confidencialidad</a></li>
+                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">Protección de denunciantes</a></li>
+                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">Seguridad en el sistema</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Contactos</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">Soporte</a></li>
+                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">FAQ (Preguntas frecuentes)</a></li>
+                        <li><a class="dropdown-item" href="" data-bs-toggle="modal" data-bs-target="#loginModal">Redes sociales</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <div>
+                <a class="btn btn-outline-light me-2" data-bs-toggle="modal" data-bs-target="#loginModal">INICIAR SESIÓN</a>
+                <a class="btn btn-outline-light me-2" data-bs-toggle="modal" data-bs-target="#registerModal">REGISTRAR</a>
+            </div>
+        </div>
+    </nav>
